ae0b2dcc410bad1efa56240372b0a9ca856ff00e
Abhishek Kumar Singh <shubhi0408@gmail.com>
2020-02-28 00:35:08 +0530

    Redirect by role - redirect to 403 page, address nomenclature, define constants

--- wp-content/plugins/cm-redirect-by-role/cm-redirect-by-role.php
+++ wp-content/plugins/cm-redirect-by-role/cm-redirect-by-role.php
@@ -1,27 +1,46 @@
 <?php
 /*
-Plugin Name: Redirect Users by Role
+Plugin Name: Restrict Content by Role
 Plugin URI:
-Description: Redirects users based on their role
-Version: 1.0
+Description: Restricts content by redirecting users based on their role
+Version: 1.1
 Author: Rahul Biswas
 Author URI: http://test.com
 License: GPLv2 or later
 */
+
+/*
+Cases for:
+    1. functions - snake_case
+    2. Constants - CAPITALISED_SNAKE_CASE
+    3. scoped variables - camelCase
+*/
+
+if ( !defined('CT_LTD') ) {
+    define ( 'CT_LTD', 'ct_ltd' );
+}
+if ( !defined('CT_LTD_ARR') ) {
+    define ( 'CT_LTD_ARR', [
+        'editor',
+        'administrator',
+        'author',
+        CT_LTD
+    ]);
+}
  
 
-if( !function_exists('cm_createRole') ) {
-   function cm_createRole(){    
-        add_role( 'custom_user1', 'Custom User 1', array( 'read' => true, 'level_0' => true ) );    
+if( !function_exists('cm_createrole') ) {
+   function cm_createrole(){    
+        add_role( CT_LTD, 'CT Limited', array( 'read' => true, 'level_0' => true ) );    
    }
-   register_activation_hook( __FILE__, 'cm_createRole' );
+   register_activation_hook( __FILE__, 'cm_createrole' );
 }
 
-if( !function_exists('cm_removeRole') ) {
-    function cm_removeRole(){ 
+if( !function_exists('cm_removerole') ) {
+    function cm_removerole(){ 
         remove_role( 'custom_user1' );
     }
-    register_deactivation_hook( __FILE__, 'cm_removeRole' );
+    register_deactivation_hook( __FILE__, 'cm_removerole' );
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -33,11 +52,11 @@ if( !function_exists('custom_meta_box_markup') ) {
         <label>
             <input type="checkbox" name="myCheckbox"            
             <?php 
-                if ( get_post_meta( get_the_ID(), 'restricTEdURL', true ) ){
+                if ( get_post_meta( get_the_ID(), 'restrictedURL', true ) ){
                     echo "checked";
                 }            
             ?>            
-            >Add URL
+            >Make it forbidden for the mortals
         </label>
     </div>
 
@@ -47,7 +66,7 @@ if( !function_exists('custom_meta_box_markup') ) {
 
 if( !function_exists('add_custom_meta_box') ) {
     function add_custom_meta_box(){
-        add_meta_box("demo-meta-box", "My Meta Box", "custom_meta_box_markup", "post", "side", "high", null);
+        add_meta_box("demo-meta-box", "Restrict Content", "custom_meta_box_markup", array("post", "page", "media", "product"), "side", "high", null);
     }  
     add_action("add_meta_boxes", "add_custom_meta_box"); 
 }
@@ -57,10 +76,10 @@ function save_metabox_data( $post_id ) {
     
     if(isset($_POST["myCheckbox"])) {        
         $postId = get_the_ID();
-        update_post_meta( $post_id = $postId, $key = 'restricTEdURL', $value = 'true' );        
+        update_post_meta( $post_id = $postId, $key = 'restrictedURL', $value = 'true' );        
     }else{
         $postId = get_the_ID();
-        delete_post_meta($post_id = $postId, "restricTEdURL");
+        delete_post_meta($post_id = $postId, "restrictedURL");
     }
      
 }
@@ -71,34 +90,20 @@ add_action( 'edit_post', 'save_metabox_data' );
 /**
  * @uses wp_get_current_user()          Returns a WP_User object for the current user
  */
-if( !function_exists('restricUsers') ) {
-    function restricUsers($user_id){
-
-        // $user = wp_get_current_user();
-        // $allowed_roles = array('editor', 'administrator', 'author');        
-        // if( !array_intersect($allowed_roles, $user->roles ) ) {
-        //     echo "This user has no role";
-        // }       
-
-        // global $post;
-        // $pID = $post->ID;
-        // if(get_post_meta( $pID, 'restricTEdURL', true )){
-            
-        // }
+if( !function_exists('restrict_users') ) {
+    function restrict_users($user_id){
 
         global $post;
-        $pID = $post->ID;
-        if(get_post_meta( $pID, 'restricTEdURL', true )){
-            $user = wp_get_current_user();
-            $allowed_roles = array('editor', 'administrator', 'author');        
-            if( !array_intersect($allowed_roles, $user->roles ) ) {
-                //echo "sdsdsdsdsdsdsdsd";
-                $url = get_permalink('66');
+        $postID = $post->ID;
+        if(get_post_meta( $postID, 'restrictedURL', true )){
+            $user = wp_get_current_user();     
+            if( !array_intersect(CT_LTD_ARR, $user->roles ) ) {
+                $url = get_permalink('2255');
                 wp_redirect($url);
             }
         }
     }
-    add_action("wp_load", "restricUsers"); 
+    add_action("wp", "restrict_users"); 
 }
 
 
